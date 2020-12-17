60a3cfa22c80d9ae6b44ac1873719c1c6ceb4dd9
evyataramar <75308585+evyataramar@users.noreply.github.com>
2020-12-17 06:58:34 +0200

    Add files via upload

--- client/Client.java
+++ client/Client.java
@@ -1,19 +1,22 @@
 package micTest;
 
 
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
 public class Client extends Thread{
 	public static int numOfClient;
+	public static volatile boolean keypressed;
 	
 
 	
-	
 	public void run() {
 		try {	
-		    while(true) {
+		    while(!Client.keypressed) {
 		    		Random randId = new Random();
 		    	    int id = randId.nextInt(Client.numOfClient);
 		    	    String urlString = "http://localhost:8080/?ClientID=" + id;
@@ -27,7 +30,7 @@ public class Client extends Thread{
 		    		
 		    		int code = con.getResponseCode();
 		    		if(code!=200) {
-		    	//	System.out.println(code /*+ " " + new String(con.getInputStream().readAllBytes(), StandardCharsets.UTF_8)*/ );
+		    		//System.out.println(code /*+ " " + new String(con.getInputStream().readAllBytes(), StandardCharsets.UTF_8)*/ );
 		    			
 		    		}
 		    }
@@ -39,17 +42,18 @@ public class Client extends Thread{
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
+		Client.keypressed = false;
 	    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 	    System.out.println("Enter the num of httpClient");
 	    int numHttpCliet = Integer.parseInt(myObj.nextLine());  // Read user input
-	    myObj.close();
 	    Client.numOfClient = numHttpCliet;
 	    for(int i=0;i<numHttpCliet;i++) {
 	    	Client c = new Client();
 	    	c.start();
 	    }
-	   
+	    while(myObj.nextLine()==null) {}//key press to finish
+	    Client.keypressed = true;
+	    myObj.close();
 	}
 
 
